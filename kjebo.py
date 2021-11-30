0007a85d07c18726e145d4967edafdd03c9311ae
kjebo <kjellbovin@hotmail.com>
2021-11-30 11:53:14 +0100

    Changes in kjebo.py

--- kjebo.py
+++ kjebo.py
@@ -1,10 +1,19 @@
-#txt = 'Sombody said something to Samantha'
-# new_txt = txt.replace('s', 'x'
+
+#def translate():
+#    string = input("Enter some text: ")
+#    new_string = string.replace('a', '4').replace('b', '8').replace('e', '3').replace(
+#        'l', '1').replace('o', '0').replace('s', '5').replace('t', '7')
+#    print(new_string)
+
 def translate():
+    replacements = ( ('a','4'), ('b','8'), ('e','3'), ('l','1'),
+                 ('o','0'), ('t','7') )
     string = input("Enter some text: ")
-    new_string = string.replace('a', '4').replace('b', '8').replace('e', '3').replace(
-        'l', '1').replace('o', '0').replace('s', '5').replace('t', '7')
-    print(new_string)
+    
+    for old, new in replacements:
+        string = string.replace(old, new)
+
+    print(string)
 
 
 """

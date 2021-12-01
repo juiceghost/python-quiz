7e195f26665d40be9131768babacf499cdba6159
Daniel12929 <daniel.renner@chasacademy.se>
2021-12-01 17:07:20 +0100

    Homework

--- renda.py
+++ renda.py
@@ -1,1 +1,15 @@
-print("Hello")
+#Ex1
+import random
+
+def roll():
+    return random.randint(1,6)
+
+#Ex2
+total = 0
+
+for i in range(10000):
+    total += roll()
+
+avg_roll = float(total) / 10000
+
+print(avg_roll)
